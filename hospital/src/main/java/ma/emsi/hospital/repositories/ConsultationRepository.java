2339295965f4e7d9b318409ebd4e93ebac624406
ManalHajj <81756572+ManalHajj@users.noreply.github.com>
2022-03-27 22:52:07 +0000

    Delete hospital directory

--- hospital/src/main/java/ma/emsi/hospital/repositories/ConsultationRepository.java
+++
@@ -1,8 +0,0 @@
-package ma.emsi.hospital.repositories;
-
-import ma.emsi.hospital.entities.Consultation;
-import ma.emsi.hospital.entities.RendezVous;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsultationRepository extends JpaRepository<Consultation,Long> {
-}
