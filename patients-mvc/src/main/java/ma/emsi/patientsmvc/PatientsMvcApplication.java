2c7b9193e382507d2425ca1183daa565902f0425
ManalHajj <81756572+ManalHajj@users.noreply.github.com>
2022-04-17 17:13:35 +0000

    Add files via upload

--- patients-mvc/src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
+++ patients-mvc/src/main/java/ma/emsi/patientsmvc/PatientsMvcApplication.java
@@ -2,10 +2,13 @@ package ma.emsi.patientsmvc;
 
 import ma.emsi.patientsmvc.entities.Patient;
 import ma.emsi.patientsmvc.repositories.PatientRepository;
+import ma.emsi.patientsmvc.security.service.SecurityService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -15,6 +18,12 @@ public class PatientsMvcApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(PatientsMvcApplication.class, args);
+
+    }
+    //au demarage cree un password encoder puis place le dans ton context
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     //@Bean
@@ -31,4 +40,24 @@ public class PatientsMvcApplication {
         };
 
     }
+    //@Bean
+    CommandLineRunner saveUsers(SecurityService securityService){
+        return args -> {
+            securityService.saveNewUser("mohamed","1234","1234");
+            securityService.saveNewUser("yasmine","1234","1234");
+            securityService.saveNewUser("hassan","1234","1234");
+
+
+            securityService.saveNewRole("USER","");
+            securityService.saveNewRole("ADMIN","");
+
+            securityService.addRoleToUser("mohamed","USER");
+            securityService.addRoleToUser("mohamed","ADMIN");
+            securityService.addRoleToUser("yasmine","USER");
+            securityService.addRoleToUser("hassan","USER");
+
+
+        };
+    }
+
 }
